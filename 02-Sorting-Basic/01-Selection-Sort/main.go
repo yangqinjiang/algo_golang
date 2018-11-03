a8ffd93e2f1f9014a27ac30db0017885f0f89935
yangqinjiang-macbookpro <yangqinjiang@qq.com>
2018-11-03 15:41:50 +0800

    d

---
+++ 02-Sorting-Basic/01-Selection-Sort/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+/**
+交换数组或切片的两个元素
+*/
+func swap(a, b *int) {
+	*a, *b = *b, *a
+}
+
+/**
+选择排序
+*/
+func selectionSort(arr []int, n int) {
+
+	for i := 0; i < n; i++ {
+		//寻找[i, n)区间里的最小值
+		mi := i
+		for j := i + 1; j < n; j++ {
+			if arr[j] < arr[mi] {
+				mi = j
+			}
+		}
+
+		swap(&arr[i], &arr[mi])
+
+	}
+
+}
+
+func main() {
+	a := []int{10, 90, 8, 7, 6, 50, 4, 31, 2, 101, 10}
+	fmt.Println(a)
+	fmt.Printf("running...\n")
+	selectionSort(a, 11)
+	fmt.Printf("done\n")
+	fmt.Println(a)
+
+}
