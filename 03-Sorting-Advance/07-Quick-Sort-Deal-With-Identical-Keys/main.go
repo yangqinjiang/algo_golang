1609a11b35eebf2c5fefa14fe1802ecbb73dc8ac
yangqinjiang-macbookpro <yangqinjiang@qq.com>
2018-11-15 22:32:17 +0800

    双路快速排序法

---
+++ 03-Sorting-Advance/07-Quick-Sort-Deal-With-Identical-Keys/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	core2 "algo_golang/03-Sorting-Advance/core"
+	"algo_golang/02-Sorting-Basic/core"
+)
+
+func main() {
+
+	fmt.Println("running Quick Sort 2 part")
+
+	// 比较Merge Sort和Quick Sort两种排序算法的性能效率
+	// 两种排序算法虽然都是O(nlogn)级别的, 但是Quick Sort算法有常数级的优势
+	// Quick Sort要比Merge Sort快, 即使我们对Merge Sort进行了优化
+
+	n := 1000000
+	ms := new(core2.MergeSort)
+	qs := new(core2.QuickSort)
+	qs_advance := new(core2.QuickSortAdvance)
+	qs_2_part := new(core2.QuickSortTwoPartition)
+	// 测试1 一般性测试
+	fmt.Printf("Test for random array,size = %d , random range [0,%d]\n", n, n)
+
+	st := new(core.SortTestHelper)
+	arr1 := st.GenerateRandomArray(n, 0, n)
+	arr2 := st.CopyIntArray(arr1, n)
+	arr3 := st.CopyIntArray(arr1, n)
+	arr4 := st.CopyIntArray(arr1, n)
+
+	st.TestSort("Merge Sort MergeSortBUAdvance", ms.MergeSortBUAdvance, arr1, n)
+	st.TestSort("Quick Sort", qs.QuickSort, arr2, n)
+	st.TestSort("Quick Sort advance", qs_advance.QuickSort, arr3, n)
+	st.TestSort("Quick Sort 2 part", qs_2_part.QuickSort, arr4, n)
+
+	fmt.Println("-------------")
+	// 测试2 测试近乎有序的数组
+	// 加入了随机选择标定点的步骤后, 我们的快速排序可以轻松处理近乎有序的数组
+	// 但是对于近乎有序的数组, 其效率比优化后的归并排序要低, 但完全再容忍范围里
+	// 思考一下为什么对于近乎有序的数组, 快排的性能比优化后的归并排序低? :)
+	swapTimes := 100
+	fmt.Printf("Test for nearly ordered array, size = %d , swap time =  [0,%d]\n", n, swapTimes)
+	arr1 = st.GenerateNearlyOrderedArray(n, swapTimes)
+	arr2 = st.CopyIntArray(arr1, n)
+	arr3 = st.CopyIntArray(arr1, n)
+	arr4 = st.CopyIntArray(arr1, n)
+
+	st.TestSort("Merge Sort MergeSortBUAdvance", ms.MergeSortBUAdvance, arr1, n)
+	st.TestSort("Quick Sort", qs.QuickSort, arr2, n)
+	st.TestSort("Quick Sort advance", qs_advance.QuickSort, arr3, n)
+	st.TestSort("Quick Sort 2 part", qs_2_part.QuickSort, arr4, n)
+
+	fmt.Println("-------------")
+	// 测试3 测试存在包含大量相同元素的数组
+	// 但此时, 对于含有大量相同元素的数组, 我们的快速排序算法再次退化成了O(n^2)级别的算法
+	// 思考一下为什么在这种情况下, 快排退化成了O(n^2)的算法? :)
+	swapTimes = 10
+	fmt.Printf("Test for random array, size = %d ,, random range [0,%d]\n", n, swapTimes)
+	arr1 = st.GenerateRandomArray(n, 0,swapTimes)
+	arr2 = st.CopyIntArray(arr1, n)
+	arr3 = st.CopyIntArray(arr1, n)
+	arr4 = st.CopyIntArray(arr1, n)
+
+	st.TestSort("Merge Sort MergeSortBUAdvance", ms.MergeSortBUAdvance, arr1, n)
+	st.TestSort("Quick Sort", qs.QuickSort, arr2, n)
+	st.TestSort("Quick Sort advance", qs_advance.QuickSort, arr3, n)
+	st.TestSort("Quick Sort 2 part", qs_2_part.QuickSort, arr4, n)
+
+}
---
+++ 03-Sorting-Advance/core/QuickSortTwoPartition.go
@@ -0,0 +1,87 @@
+package core
+
+import (
+	"math/rand"
+	"time"
+	"algo_golang/02-Sorting-Basic/core"
+)
+/**
+双路快速排序法
+// 调用双路快速排序的partition
+ */
+type QuickSortTwoPartition struct {
+	myrand *rand.Rand  //随机数生成器
+}
+/**
+交换数组或切片的两个元素
+*/
+func (e *QuickSortTwoPartition) swap(a, b *int) {
+	*a, *b = *b, *a
+}
+// 对arr[l...r]部分进行partition操作
+// 返回p, 使得arr[l...p-1] < arr[p] ; arr[p+1...r] > arr[p]
+func (e *QuickSortTwoPartition)__partition(arr []int, l,r int) int  {
+	// 随机在arr[l...r]的范围中, 选择一个数值作为标定点pivot
+	e.swap( &arr[l] , &arr[e.myrand.Int()%(r-l+1)+l] );
+
+	v:=arr[l]
+	// arr[l+1...i) <= v; arr(j...r] >= v
+	i:=l+1
+	j:=r
+	for{
+		// 注意这里的边界, arr[i] < v, 不能是arr[i] <= v
+		// 思考一下为什么?
+		for{
+			if i<=r && arr[i] < v{
+				i++
+			}else{
+				break
+			}
+		}
+
+		// 注意这里的边界, arr[j] > v, 不能是arr[j] >= v
+		// 思考一下为什么?
+
+		for {
+			if j>= l+1 && arr[j] > v{
+				j--
+			}else{
+				break
+			}
+		}
+
+		// 对于上面的两个边界的设定, 有的同学在课程的问答区有很好的回答:)
+		// 大家可以参考: http://coding.imooc.com/learn/questiondetail/4920.html
+
+		if i> j{
+			break
+		}
+
+		e.swap(&arr[i],&arr[j])
+		i++
+		j--
+	}
+	e.swap(&arr[l],&arr[j])
+
+
+	return j
+}
+
+// 对arr[l...r]部分进行快速排序
+func (e *QuickSortTwoPartition)__quickSort(arr []int, l,r int)  {
+
+	// 对于小规模数组, 使用插入排序进行优化
+	if l >= r{
+		iss := new(core.InsertionSort)
+		iss.InsertionSortLR(arr,l,r)
+		return
+	}
+	// 调用双路快速排序的partition
+	p := e.__partition(arr,l,r)
+	e.__quickSort(arr,l,p-1)
+	e.__quickSort(arr,p+1,r)
+}
+func (e *QuickSortTwoPartition)QuickSort(arr []int, n int)  {
+	e.myrand = rand.New(rand.NewSource(time.Now().UnixNano()))
+	e.__quickSort(arr,0,n-1)
+}
