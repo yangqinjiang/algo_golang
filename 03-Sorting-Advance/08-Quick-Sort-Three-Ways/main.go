84c6692340b0620907c492ec3ae0139a6896d33c
yangqinjiang-macbookpro <yangqinjiang@qq.com>
2018-11-15 22:50:02 +0800

    三路快速排序法

--- 03-Sorting-Advance/08-Quick-Sort-Three-Ways/main.go
+++ 03-Sorting-Advance/08-Quick-Sort-Three-Ways/main.go
@@ -8,30 +8,32 @@ import (
 
 func main() {
 
-	fmt.Println("running Quick Sort 2 ways")
+	fmt.Println("running Quick Sort 3 ways")
 
-	// 比较Merge Sort和Quick Sort两种排序算法的性能效率
-	// 两种排序算法虽然都是O(nlogn)级别的, 但是Quick Sort算法有常数级的优势
-	// Quick Sort要比Merge Sort快, 即使我们对Merge Sort进行了优化
+	// 比较Merge Sort和双路快速排序和三路快排三种排序算法的性能效率
+	// 对于包含有大量重复数据的数组, 三路快排有巨大的优势
+	// 对于一般性的随机数组和近乎有序的数组, 三路快排的效率虽然不是最优的, 但是是在非常可以接受的范围里
+	// 因此, 在一些语言中, 三路快排是默认的语言库函数中使用的排序算法。比如Java:)
 
 	n := 1000000
 	ms := new(core2.MergeSort)
-	qs := new(core2.QuickSort)
 	qs_advance := new(core2.QuickSortAdvance)
 	qs_2_ways := new(core2.QuickSortTwoWays)
+	qs_3_ways := new(core2.QuickSortThreeWays)
 	// 测试1 一般性测试
 	fmt.Printf("Test for random array,size = %d , random range [0,%d]\n", n, n)
 
 	st := new(core.SortTestHelper)
 	arr1 := st.GenerateRandomArray(n, 0, n)
-	arr2 := st.CopyIntArray(arr1, n)
 	arr3 := st.CopyIntArray(arr1, n)
 	arr4 := st.CopyIntArray(arr1, n)
+	arr5 := st.CopyIntArray(arr1, n)
 
 	st.TestSort("Merge Sort MergeSortBUAdvance", ms.MergeSortBUAdvance, arr1, n)
-	st.TestSort("Quick Sort", qs.QuickSort, arr2, n)
+
 	st.TestSort("Quick Sort advance", qs_advance.QuickSort, arr3, n)
 	st.TestSort("Quick Sort 2 ways", qs_2_ways.QuickSort, arr4, n)
+	st.TestSort("Quick Sort 3 ways", qs_3_ways.QuickSort, arr5, n)
 
 	fmt.Println("-------------")
 	// 测试2 测试近乎有序的数组
@@ -41,14 +43,15 @@ func main() {
 	swapTimes := 100
 	fmt.Printf("Test for nearly ordered array, size = %d , swap time =  [0,%d]\n", n, swapTimes)
 	arr1 = st.GenerateNearlyOrderedArray(n, swapTimes)
-	arr2 = st.CopyIntArray(arr1, n)
 	arr3 = st.CopyIntArray(arr1, n)
 	arr4 = st.CopyIntArray(arr1, n)
+	arr5 = st.CopyIntArray(arr1, n)
 
 	st.TestSort("Merge Sort MergeSortBUAdvance", ms.MergeSortBUAdvance, arr1, n)
-	st.TestSort("Quick Sort", qs.QuickSort, arr2, n)
+
 	st.TestSort("Quick Sort advance", qs_advance.QuickSort, arr3, n)
 	st.TestSort("Quick Sort 2 ways", qs_2_ways.QuickSort, arr4, n)
+	st.TestSort("Quick Sort 3 ways", qs_3_ways.QuickSort, arr5, n)
 
 	fmt.Println("-------------")
 	// 测试3 测试存在包含大量相同元素的数组
@@ -57,13 +60,14 @@ func main() {
 	swapTimes = 10
 	fmt.Printf("Test for random array, size = %d ,, random range [0,%d]\n", n, swapTimes)
 	arr1 = st.GenerateRandomArray(n, 0,swapTimes)
-	arr2 = st.CopyIntArray(arr1, n)
 	arr3 = st.CopyIntArray(arr1, n)
 	arr4 = st.CopyIntArray(arr1, n)
+	arr5 = st.CopyIntArray(arr1, n)
 
 	st.TestSort("Merge Sort MergeSortBUAdvance", ms.MergeSortBUAdvance, arr1, n)
-	st.TestSort("Quick Sort", qs.QuickSort, arr2, n)
+
 	st.TestSort("Quick Sort advance", qs_advance.QuickSort, arr3, n)
 	st.TestSort("Quick Sort 2 ways", qs_2_ways.QuickSort, arr4, n)
+	st.TestSort("Quick Sort 3 ways", qs_3_ways.QuickSort, arr5, n)
 
 }
