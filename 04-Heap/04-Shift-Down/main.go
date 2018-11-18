c2643801d11e8fa97b49e729b33b0f2be20b2eb7
yangqinjiang-macbookpro <yangqinjiang@qq.com>
2018-11-18 14:02:41 +0800

    04-Shift-Down

---
+++ 04-Heap/04-Shift-Down/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"algo_golang/04-Heap/core"
+	"fmt"
+	"math/rand"
+	"time"
+	"github.com/smartystreets/assertions"
+)
+
+func main() {
+
+	n := 100
+	maxheap := core.NewMaxHeap(n)
+	fmt.Println("maxheap=",maxheap)
+	fmt.Println("IsEmpty? ",maxheap.IsEmpty())
+	fmt.Println("Size =",maxheap.Size())
+
+
+	//插入元素
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < n; i++ {
+		//随机生成
+		maxheap.Insert(r.Int()% 100)
+	}
+	fmt.Println("堆里的数据顺序")
+	maxheap.TestPrint()
+
+	// 将maxheap中的数据逐渐使用extractMax取出来
+	// 取出来的顺序应该是按照从大到小的顺序取出来的
+	fmt.Println("取出来的顺序")
+	arr := make([]int,n,n)
+	for i:= 0 ;i<n;i++{
+		arr[i] = maxheap.ExtractMax()
+		fmt.Print(arr[i]," ")
+	}
+	fmt.Println()
+	// 确保arr数组是从大到小排列的
+	for i:=1 ;i<n;i++{
+		assertions.ShouldBeTrue(arr[i-1] >= arr[i])
+	}
+
+}
--- 04-Heap/core/MaxHeap.go
+++ 04-Heap/core/MaxHeap.go
@@ -44,6 +44,23 @@ func (e *MaxHeap)shiftUp(k int)  {
 	}
 }
 
+func (e *MaxHeap)shiftDown(k int)  {
+
+	for 2*k <= e.count{
+		j := 2*k//在此轮循环中,data[k]和data[j]交换位置
+		if j+1 <= e.count && e.data[j+1] > e.data[j]{
+			j++
+		}
+		// data[j] 是 data[2*k]和data[2*k+1]中的最大值
+		if e.data[k] >= e.data[j] {
+			break
+		}
+
+		e.swap(&e.data[k],&e.data[j])
+		k=j
+	}
+}
+
 /**
 交换数组或切片的两个元素
 */
@@ -51,6 +68,7 @@ func (e *MaxHeap) swap(a, b *int) {
 	*a, *b = *b, *a
 }
 
+// 向最大堆中插入一个新的元素 item
 func (e *MaxHeap)Insert(item int)  {
 
 	//边界
@@ -60,6 +78,26 @@ func (e *MaxHeap)Insert(item int)  {
 	e.data[e.count + 1] = item
 	e.count ++
 	e.shiftUp(e.count)
+}
+// 从最大堆中取出堆顶元素, 即堆中所存储的最大数据
+func (e *MaxHeap)ExtractMax() int  {
+	assertions.ShouldBeTrue(e.count > 0)
+	ret := e.data[1] //读取第一个,是最大值
+
+	//交换最后和第一个元素,使得不是最大堆
+	e.swap(&e.data[1],&e.data[e.count])
+	e.count --
+	//进行 shiftDown
+	e.shiftDown(1)
+
+	//返回第一个
+	return  ret
+
+}
+
+func (e *MaxHeap)GetMax() int  {
+	assertions.ShouldBeTrue(e.count > 0)
+	return e.data[1]
 }
 
 // 以树状打印整个堆结构
