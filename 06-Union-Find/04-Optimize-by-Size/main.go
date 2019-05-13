a54fd08add9ad86ef1d9a8e7a2be779397532f66
qin江 <yangqinjiang@qq.com>
2019-05-13 14:22:38 +0800

    optimize by size

--- 06-Union-Find/04-Optimize-by-Size/main.go
+++ 06-Union-Find/04-Optimize-by-Size/main.go
@@ -13,9 +13,7 @@ func main() {
 	//虽然isConnected只需要O(1)的时间,但由于union操作需要O(n)的时间
 	//总体测试过程的算法复杂度是O(n^2)的
 	ufth := core.UnionFindTestHelper{}
-	ufth.TestUF1(n)
-
-
-	ufth.TestUF2(n)
+	//ufth.TestUF1(n)  // 100w+, 不要运行它
+	//ufth.TestUF2(n)
 	ufth.TestUF3(n)
 }
